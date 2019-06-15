388f2a106f9e067b0730080db3f0890ebfe2a1c2
n00976 <n.ordecka@gmail.com>
2019-06-15 18:04:32 +0200

    actual

--- main_menu.py
+++ main_menu.py
@@ -1,20 +1,21 @@
-#import woods
+import woods
 import tavern
 import show_pasek as sp
+import life_module
 
-main_menu_dict = {
+menu_dict = {
     'tavern': 't',
     'the woods': 'w'
 }
 
 
-def main_menu(full_name, bag):
+def menu(full_name, bag):
     print("Welcome in our game, " + full_name + " ! This is your bag:" )
     sp.show_pasek(bag)
-    for k, v in main_menu_dict.items():
+    for k, v in menu_dict.items():
         print("".join([k + " : " + str(v) + "  " ]))
-    main_menu_choice = input("choose where do you want to go:")
-    if main_menu_choice == 't':
-        tavern_adventure()
-    #if main_menu_choice == 'w':
-    #    woods_adventure()
+    menu_choice = input("choose where do you want to go:")
+    if menu_choice == 't':
+        tavern.tavern_adventure(bag, life_module.life)
+    if menu_choice == 'w':
+        woods.woods_adventure(bag, life)
