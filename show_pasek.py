cb7133cfb0a069546950396a1d25c36507fd2883
n00976 <n.ordecka@gmail.com>
2019-06-29 14:14:51 +0200

    aktualne

--- show_pasek.py
+++
@@ -1,8 +0,0 @@
-from life_module import Life
-from life_module import bcolors
-
-
-def show_pasek(bag):
-    Life.show_life()
-    print("".join([k + " : " + str(v) + "  " for k, v in bag.items()]))
-    print('\n')
