fcbd9a9b10c1f5ecb6ea60ad17fba09131c7377f
kasia6277 <katarzyna.kasprzak96@gmail.com>
2019-06-29 12:30:06 +0200

    9.

--- life_module.py
+++ life_module.py
@@ -1,6 +1,8 @@
-#import life
-import math
+#import???
+import bcolors
 import sys
+import math
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -12,31 +14,61 @@ class bcolors:
     UNDERLINE = '\033[4m'
 #bcolors.(np)Header + ... + bcolors.ENDC
 
-life = 100
 
-def show_life(life):
-    rounded = int(math.ceil(life / 10.0))
-    lost = 10 - rounded
-    print('[ ' + ('▓ ' * (rounded) + ('░ ' * int(lost)) + ']' + ' ' + str(life) + '%'))
+class Life:
 
-def print_low_life(life):
-    if life <= 30:
-        print("You are slowly dying, get some rest at Karczma??")
-    else:
-        pass
+    def __init__(self, life):
+        self.life = life
 
-def end_game(life):
-    if life <= 0:
-        sys.exit(0)
-    else:
-        pass
+    def show_life(self):
+        """ printing battery"""
+        rounded = int(math.ceil(self.life / 10.0))
+        lost = 10 - rounded
+        if self.life >= 70:
+            battery = (bcolors.OKGREEN + '[ ' + ('▓ ' * (rounded) + bcolors.ENDC + ('░ ' * int(lost)) + ']' + ' ' + str(self.life) + '%'))
+            print(battery)
 
-def main(life):
-    if life > 30:
-        show_life(life)
-    elif life <= 30 and life > 0:
-        show_life(life)
-        print_low_life(life)
-    else:
-        print("You died!")
-        end_game(life)
+        elif self.life <= 30:
+            battery = (bcolors.FAIL +'[ ' + ('▓ ' * (rounded) + bcolors.ENDC + ('░ ' * int(lost)) + ']' + ' ' + str(self.life) + '%'))
+            print(battery)
+
+        elif self.life >30 and self.life <70:
+            battery = (bcolors.WARNING +'[ ' + ('▓ ' * (rounded) + bcolors.ENDC + ('░ ' * int(lost)) + ']' + ' ' + str(self.life) + '%'))
+            print(battery)
+        else:
+            pass
+
+
+    def print_low_life(self):
+        """printing if you have less than 3 life"""
+        if self.life <= 30:
+            return "You are slowly dying, get some rest at Karczma??"
+        else:
+            pass
+
+    def end_game(self):
+        """if you have no life: printing youre dead"""
+        if self.life <= 0:
+            print("You're dead :(")
+            sys.exit(0)
+        else:
+            pass
+
+    def __repr__(self):
+        """do modules from above"""
+        if self.life > 30:
+            self.show_life()
+            print(self.show_life())
+        elif self.life <= 30 and self.life > 0:
+            self.show_life()
+            self.print_low_life()
+            print(self.show_life(), self.print_low_life())
+        return " "
+
+
+if __name__ == '__main__':
+    zycie = Life(40)
+    print(zycie)
+    print("odpaliłeś jako głowny moduł")
+else:
+    print("odpaliłeś moduł")
